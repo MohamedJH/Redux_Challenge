8cd18cf8d0f45d672e48c7823912c98c0ba74a27
MohamedJH <mohamedjahha0@gmail.com>
2021-10-30 12:47:43 +0100

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import Todoform from './Components/Todo forms Component/Todoform'
+import Todolist from './Components/Todo list component/Todolist'
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card bg-light" style={{ width: "1100px", margin: "50px auto" }}>
+        <Todoform  />
+        <Todolist />
+      </div>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
+
